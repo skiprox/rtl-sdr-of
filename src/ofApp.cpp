819382ac5a3c3d73d26097b30b8570ce3f52c3e3
skiprox <seanmichaelscanlan@gmail.com>
2019-02-27 12:11:28 -0500

    Doing mesh interpretations of radio data

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,5 +1,4 @@
 #include "ofApp.h"
-#include "rtl-sdr.h"
 
 #define FREQUENCY (107.7 * 1000000)
 #define SAMPLE_RATE 900001
@@ -7,22 +6,121 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	uint32_t deviceCount = rtlsdr_get_device_count();
+	ofBackground(0);
+	float width = ofGetWidth();
+	float height = ofGetHeight();
+	int deviceCount = rtlsdr_get_device_count();
 	cout << "RTL Device Count: " << deviceCount << endl;
+	// setup the rtl sdr dongle
+	if (deviceCount > 0) {
+		std::cout << std::endl << "Opening device 0." << std::endl;
+
+		std::cout << "(RET) rtlsdr_open = " << rtlsdr_open(&device, 0) << std::endl;
+		std::cout << "(RET) rtlsdr_set_freq_correction = " << rtlsdr_set_freq_correction(device, 68) << std::endl;
+		std::cout << "(RET) rtlsdr_set_center_freq = " << rtlsdr_set_center_freq(device, FREQUENCY) << std::endl;
+		std::cout << "(RET) relsdr_set_sample_rate = " << rtlsdr_set_sample_rate(device, SAMPLE_RATE) << std::endl;
+
+		std::cout << "(RET) rtlsdr_reset_buffer = " << rtlsdr_reset_buffer(device) << std::endl;
+	}
+	int rowsColsVal = floor(sqrt(BYTES_TO_READ / 2 - 1));
+	cout << "HOW MANY BITCH" << rowsColsVal << endl;
+	// create the mesh
+	for (int r = 0; r<rowsColsVal; r++){
+	    for (int c = 0; c<rowsColsVal; c++){
+	        
+	        glm::vec3 pos;      // grid centered at 0,0,0
+	        
+	        float halfWidth     = width * 0.5;
+	        float halfHeight    = height * 0.5;
+	        
+	        pos.x = ofMap(r, 0, rowsColsVal-1, -halfWidth, halfWidth);
+	        pos.y = ofMap(c, 0, rowsColsVal-1, halfHeight, -halfHeight);    // Y+ is up in 3D!
+	        pos.z = 0;    // add depth later
+	        
+	        // add the point to the mesh
+	        mesh.addVertex(pos);
+	        
+	        // add a color for the point
+	        mesh.addColor(ofColor());
+
+	        if (r > 0 && c > 0) {
+	            int index = r * rowsColsVal + c;
+	            // triangle 1
+	            mesh.addIndex(index);               // A    - this pt
+	            mesh.addIndex(index - 1);           // B    - + col
+	            mesh.addIndex(index - rowsColsVal);        // C    - + row
+	            
+	            // triangle 2
+	            mesh.addIndex(index - 1);           // B
+	            mesh.addIndex(index - 1 - rowsColsVal);    // D
+	            mesh.addIndex(index - rowsColsVal);        // C
+	        }
+	    }
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+	// Read sync
+	rtlsdr_read_sync(device, buffer, BYTES_TO_READ, &bytesRead);
 
+	// reset these variables to 0
+	iIdx = 0;
+	qIdx = 0;
+	// Construct the phase data array
+	for (int i = 0; i < BYTES_TO_READ; i += 2) {
+		iData[iIdx++] = buffer[i];
+		qData[qIdx++] = buffer[i + 1];
+	}
+	for (int i = 0; i < BYTES_TO_READ / 2; ++i) {
+		phaseData[i] = atan2(iData[i], qData[i]);
+	}
+
+	// cout << "[";
+	// for (int i = 1; i < BYTES_TO_READ / 2 - 1; i++) {
+	// 	cout << (phaseData[i] - phaseData[i - 1]) * 1000.f << ",";
+	// }
+	// cout << "]" << endl;
+	// cout << "WE READ ONE PIECE OF PHASE DATA" << endl;
+	updateZValue();
+	updateColors();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	cam.begin();
+    ofEnableDepthTest();
+    mesh.drawWireframe();
+    ofDisableDepthTest();
+    cam.end();
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+// --------------------------------
+void ofApp::updateZValue(){
+	for (int i = 0; i < mesh.getVertices().size(); i++) {
+		glm::vec3& vertex = mesh.getVertices()[i];
+		vertex.z = (phaseData[i] - phaseData[i - 1]) * 1000.f;
+	}
+}
+
+// --------------------------------
+void ofApp::updateColors(){
+    
+    // map colors based on vertex z / depth
+    
+    for (int i=0; i<mesh.getVertices().size(); i++){
+        
+        // 1 color per vertex
+        glm::vec3& vertex = mesh.getVertices()[i];
+        
+        // get depth as percent of noise range
+        float depthPercent = ofMap(vertex.z, -noiseAmp, noiseAmp, 0, 1, true);    // map 0-1
+
+        // lerp color
+        ofColor color = colorFar.getLerped( colorNear, depthPercent );
+        
+        mesh.setColor(i, color);        // set mesh color
+    }
 
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "ofMain.h"
+#include "rtl-sdr.h"
+
+#define BYTES_TO_READ (0xFFFF + 1)
 
 class ofApp : public ofBaseApp{
 
@@ -9,6 +12,26 @@ class ofApp : public ofBaseApp{
 		void update();
 		void draw();
 
-		void keyReleased(int key);
+		void updateZValue();
+		void updateColors();
+
+		// called in setup
+		unsigned char buffer[BYTES_TO_READ];
+		int bytesRead;
+		rtlsdr_dev_t *device;
+		double phaseData[BYTES_TO_READ / 2];
+
+		// mesh stuff
+		ofEasyCam cam;
+		ofMesh mesh;
+		ofColor colorNear = ofColor::fuchsia;
+		ofColor colorFar = ofColor::navy;
+		float noiseAmp = 40.f;
+
+		// not called until update
+		int iIdx = 0;
+		unsigned char iData[BYTES_TO_READ / 2];
+		int qIdx = 0;
+		unsigned char qData[BYTES_TO_READ / 2];
 		
 };
